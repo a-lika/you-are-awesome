a2b0c0d25e541940324386fc6fd751c1acf6b827
Anzhelika <anzhelika110k@gmail.com>
2018-10-05 20:17:30 +0300

    solution: 9/10 tests pass

--- src/index.js
+++ src/index.js
@@ -1,18 +1,78 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (propertyName) => {
+    return propertyName;
+};
+const createNotEnumerableProperty = (propertyName) => {
+    return Symbol(propertyName);
+};
+
+const createProtoMagicObject = () => {
+    function Constr() { };
+    let magic = new Constr;
+    magic.prototype = magic.__proto__;
+    return magic;
+};
+
+let counter = 0;
+let incrementor = () => {
+    incrementor = function () {
+        counter += 1;
+        return incrementor;
+    };
+    incrementor.toString = function () {
+        return counter;
+    };
+    return incrementor();
+};
+
+let asyncCounter = 0;
+let asyncIncrementor = () => {
+    asyncCounter += 1;
+    return new Promise(resolve => {
+        resolve(asyncCounter);
+    })
+};
+
+function* createIncrementer() {
+    let index = 1;
+    while (index <= 3)
+        yield index++;
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+const returnBackInSecond = param => {
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            resolve(param);
+        }, 1000);
+    })
+};
+
+const getDeepPropertiesCount = obj => {
+    let count = 0;
+    let object = obj;
+
+    function addCount(object) {
+        for (let key in object) {
+            if (typeof (object[key]) === 'object') {
+                count += 1;
+                addCount(object[key])
+            }
+        }
+        return count;
+    }
+    return addCount(object);
+};
+
+const createSerializedObject = () => {
+    return Object('object');
+};
+
+const toBuffer = () => { };
+const sortByProto = (arr) => {
+    return arr.sort((a, b) => { a - b });
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
